ea97f98485177d96e612bbb1d82b050945086f0f
frbl <frank.blaauw@gmail.com>
2014-09-30 20:24:59 +0200

    Added websockets

---
+++ public/javascripts/services/socket_service.js
@@ -0,0 +1,25 @@
+wacc2014App.factory('socket', function ($rootScope) {
+  var socket = io.connect();
+
+  // Copied from http://www.ibm.com/developerworks/library/wa-nodejs-polling-app/
+  return {
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+  };
+});
