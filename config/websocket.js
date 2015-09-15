41d09634f515d01af67f79f6cde45b23676ecf2e
Frank Blaauw <frank.blaauw@gmail.com>
2015-09-15 10:23:55 +0200

    Removed websockets

--- config/websocket.js
+++ config/websocket.js
@@ -1,11 +1,7 @@
 var http = require('http');
-var message_socket_controller = require('../app/controllers/message_socket_controller');
 
 module.exports = function (app) {
   var server = http.createServer(app);
-  var io = require('socket.io').listen(server);
-
-  io.sockets.on('connection', message_socket_controller.manage_messages);
 
   app.set('port', process.env.PORT || 3000);
 
