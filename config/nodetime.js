118fa5f325f9a8b03de938d65d4986c30446dcb3
Frank Blaauw <frank.blaauw@gmail.com>
2019-09-08 21:26:39 +0200

    No websockets, update some package

--- config/nodetime.js
+++ config/nodetime.js
@@ -1,6 +1,6 @@
-if(process.env.NODETIME_ACCOUNT_KEY) {
+if (process.env.NODETIME_ACCOUNT_KEY) {
   require('nodetime').profile({
     accountKey: process.env.NODETIME_ACCOUNT_KEY,
     appName: 'My Application Name' // optional
-  });
+  })
 }
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,16 +1,16 @@
 var wacchatApp = angular.module('wacchat', ['ngRoute',
-                                              'wacchatControllers',
-                                              'wacchatServices']);
+  'wacchatControllers',
+  'wacchatServices'])
 
-var wacchatControllers = angular.module('wacchatControllers', []);
-var wacchatServices = angular.module('wacchatServices', ['ngResource']);
+var wacchatControllers = angular.module('wacchatControllers', [])
+var wacchatServices = angular.module('wacchatServices', ['ngResource'])
 
 wacchatApp.config(['$routeProvider',
-  function($routeProvider) {
-  $routeProvider
-    .otherwise({
-      redirectTo:'/',
-      controller:'ChatController',
-      templateUrl:'views/chat.html'
-    });
-}]);
+  function ($routeProvider) {
+    $routeProvider
+      .otherwise({
+        redirectTo: '/',
+        controller: 'ChatController',
+        templateUrl: 'views/chat.html'
+      })
+  }])
